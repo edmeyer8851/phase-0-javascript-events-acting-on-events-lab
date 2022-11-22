483b485495065ba543d29108175e2023b8c0d150
Ed Meyer <edmeyer8851@gmail.com>
2022-11-22 11:54:42 -0500

    commit tree 1d4cfc1a634a3c8c9e69b8a59df07f253806207c

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 2}px`;
+    }
+  }
+  
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 2}px`;
+    }
+}
+
+function moveDodgerUp(){
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+
+    if (bottom < 380){
+        dodger.style.bottom = `${bottom + 2}px`;
+    }
+}
+
+function moveDodgerDown(){
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+
+    if (bottom > 0){
+        dodger.style.bottom = `${bottom - 2}px`;
+    }
+}
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+  });
+  
